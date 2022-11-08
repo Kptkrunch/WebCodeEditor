d2e3f4f6a8b971cbe8c633e9ffb2936da56517ff
Joshua T Edwards <Kptkrunch@users.noreply.github.com>
2022-11-07 19:30:03 -0600

    added esbuild.wasm file to public folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,39 @@
+import * as esbuild from 'esbuild-wasm';
 import { createRoot } from "react-dom/client";
+import { useState, useEffect } from "react";
+
 
 const App = () => {
 
-    return (<div>
-        <h1>
-            Sample text!!!
-        </h1>
-    </div>
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+
+    const startService = async () => {
+        const service = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        })
+        console.log(service);
+    }
+
+    useEffect(() => {
+        startService();
+    }, []);
+
+    const onClick = () => {
+        console.log(input);
+    }
+
+    return (
+        <div>
+            <textarea value={input} onChange={event => setInput(event.target.value)}></textarea>
+            <div>
+                <button onClick={onClick}>Submit</button>
+            </div>
+            <pre>{code}</pre>
+        </div>
     )
 }
 
-const root = createRoot(document.querySelector('root') as HTMLElement);
+const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
